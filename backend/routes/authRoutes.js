74a99c73d64be6c6199c257d0f8da6fd9999872f
virajdana <virajdananjaya20001@gmail.com>
2026-07-23 21:24:19 +0530

    Creating Authentication APIs

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { registerUser, loginUser, getUserProfile, updateUserProfile } = require("../controllers/authController");
+const { protect } = require("../middlewares/authMiddleware");
+
+const router = express.Router();
+
+// Auth Routes
+router.post("/register", registerUser); // Register User
+router.post("/login", loginUser); // Login User
+router.get("/profile", protect, getUserProfile); // Get User Profile
+router.put("/profile", protect, updateUserProfile); // Update Profile
+
+module.exports = router;
