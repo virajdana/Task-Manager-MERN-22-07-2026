1af4db18a8da8a5297c0680bb1c1f2641ed2a296
virajdana <virajdananjaya20001@gmail.com>
2026-07-29 21:41:23 +0530

    Creating Task Distribution Section (Pie Chart)

---
+++ frontend/src/components/Charts/CustomPieChart.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer, Legend } from "recharts";
+import CustomTooltip from './CustomToolTip';
+
+const CustomPieChart = ({ data, colors }) => {
+  return (
+    <ResponsiveContainer width="100%" height={325}>
+      <PieChart>
+        <Pie
+          data={data}
+          dataKey="count"
+          nameKey="status"
+          cx="50%"
+          cy="50%"
+          outerRadius={130}
+          innerRadius={100}
+          labelLine={false}
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+          ))}
+        </Pie>
+        <Tooltip content={<CustomTooltip />} />
+        <Legend />
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default CustomPieChart;
