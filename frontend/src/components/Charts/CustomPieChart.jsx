141ecb1b397f0cd5b9a9eff8dc2636355e234035
virajdana <virajdananjaya20001@gmail.com>
2026-07-30 17:22:58 +0530

    Dashboard Page

--- frontend/src/components/Charts/CustomPieChart.jsx
+++ frontend/src/components/Charts/CustomPieChart.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer, Legend } from "recharts";
 import CustomTooltip from './CustomToolTip';
+import CustomLegend from './CustomLegend';
 
 const CustomPieChart = ({ data, colors }) => {
   return (
@@ -21,7 +22,7 @@ const CustomPieChart = ({ data, colors }) => {
           ))}
         </Pie>
         <Tooltip content={<CustomTooltip />} />
-        <Legend />
+        <Legend content={<CustomLegend />} />
       </PieChart>
     </ResponsiveContainer>
   );
--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -11,6 +11,7 @@ import InfoCard from "../../components/Cards/InfoCard";
 import { LuArrowRight } from "react-icons/lu";
 import TaskListTable from "../../components/TaskListTable";
 import CustomPieChart from "../../components/Charts/CustomPieChart";
+import CustomBarChart from "../../components/Charts/CustomBarChart";
 
 const COLORS = ["#8D51FF", "#00B8DB", "#7BCE00"];
 
@@ -129,6 +130,7 @@ const Dashboard = () => {
 
        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-4 md:my-6">
 
+        <div>
         <div className="card">
           <div className="flex items-center justify-between">
             <h5 className="font-medium">Task Distribution</h5>
@@ -137,6 +139,18 @@ const Dashboard = () => {
             data={pieChartData}
             colors={COLORS}
           />
+        </div>
+        </div>
+
+        <div>
+        <div className="card">
+          <div className="flex items-center justify-between">
+            <h5 className="font-medium">Task Priority Levels</h5>
+          </div>
+          <CustomBarChart
+            data={barChartData}
+          />
+        </div>
         </div>
 
         <div className="md:col-span-2">
